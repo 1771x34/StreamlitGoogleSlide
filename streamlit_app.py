891e74512f95436e3918b4465c48646a905331d4
1771x34 <1771x34@gmail.com>
2023-06-28 15:58:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/edit#slide=id.g255bca974fd_0_232", height=432)
+components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s//embed?start=false&loop=false&delayms=3000", height=432)
