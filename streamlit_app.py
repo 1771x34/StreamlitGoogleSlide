fa8b5a27affa211444595735d7603f7241c3a79b
1771x34 <1771x34@gmail.com>
2023-06-28 17:20:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
-
 st.title('Presentation App')
-
-# You can use '100%' for the width and a large number for the height.
-# Please adjust based on your specific needs and tests
-components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/edit?usp=sharing", width='100%', height=800)
+components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/edit?usp=sharing", height=432)
