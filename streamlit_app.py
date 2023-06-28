c592dacf5de02035d8e1482b14f265ba91fecea7
1771x34 <1771x34@gmail.com>
2023-06-28 17:20:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title('Connection AI Presentation')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQMDmwbNHRdxIfgszl1Y99GEtle6tZpd-2a4axmqSNER8M_9q-zgOo8xGwoLITHdFMViHcB6Vx_Sai7/embed?start=false&loop=false&delayms=3000", width=None, height=432)
+
+st.title('Presentation App')
+
+# You can use '100%' for the width and a large number for the height.
+# Please adjust based on your specific needs and tests
+components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/edit?usp=sharing", width='100%', height=800)
