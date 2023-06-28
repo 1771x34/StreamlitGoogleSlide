8695a5cc040a850e02cafa1fd7cdf0845792f6f5
1771x34 <1771x34@gmail.com>
2023-06-28 16:09:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('Connection AI Presentation')
-components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/embed?start=false&loop=false&delayms=3000", height=700)
+components.iframe("https://docs.google.com/presentation/d/1AX6fU8HUXYPYoFmM8KtDcpKkhWM4Z-goAvosnHmt18s/embed?start=false&loop=false&delayms=3000", height=432)
