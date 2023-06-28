a375a92a3fc6c3e63d36286f9dd9581751cca96d
1771x34 <1771x34@gmail.com>
2023-06-28 17:43:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.markdown("<h1 style='text-align: center; color: blue;'>Presentation App</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #6495ED;'>Connect AI</h1>", unsafe_allow_html=True)
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQMDmwbNHRdxIfgszl1Y99GEtle6tZpd-2a4axmqSNER8M_9q-zgOo8xGwoLITHdFMViHcB6Vx_Sai7/embed?start=false&loop=false&delayms=3000", height=432)
